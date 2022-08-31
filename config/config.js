819af7ac20eb93b9ebd1f8e041a7ddc0b42b8e60
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-08-31 15:45:09 +0200

    last update

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ const { MONGO_URI } = require("./keys");
 const dbConnection = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("Database connected");
   } catch (error) {
     console.error(error);
     throw new Error("Failed to connect with database.");
