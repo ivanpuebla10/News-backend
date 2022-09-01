66460c04f37cc5f210056f483a73580aee950345
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-09-01 14:19:36 +0200

    finished

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const { dbConnection } = require("./config/config");
 const { typeError } = require("./middlewares/errors");
 require("dotenv").config();
+const cors = require('cors')
+
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors())
 app.use(express.static("./public"));
 
 dbConnection();
